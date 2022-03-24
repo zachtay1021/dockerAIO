811dd6b80eef92f8745797ccdd6e6bbb2821b619
zachtay1021 <zachtay1021@gmail.com>
2022-03-24 00:32:33 -0400

    FIXED CREATE!

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ def main():
                     else:
                         print('Container not found.')
 
-            elif user_choice == 'create':  # NEED TO FIGURE OUT HOW TO MOUNT VOLUMES
-                volumes = {'/': {'bind': '/mnt', 'mode': 'rw'}}
+            elif user_choice == 'create':
+                volumes = {'/': {'bind': '/', 'mode': 'rw'}}
 
                 client.containers.run(image='ubuntu', command='bash', name='ubuntu', privileged=True, remove=True, detach=True, tty=True, stdin_open=True, network_mode="host", volumes=volumes)
                 print("Container created")
